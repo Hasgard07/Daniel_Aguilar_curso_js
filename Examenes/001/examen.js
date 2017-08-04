dd0c3474c7b4d532d8adcee3a5ec19b8769b6266
Hasgard03 <daal8053@gmail.com>
2017-08-04 12:58:59 -0500

    avance
    avance

--- Examenes/001/examen.js
+++ Examenes/001/examen.js
@@ -1,4 +1,5 @@
 
+//Funciones generales
 function generarNumeroAleatorioEntre(minimo, maximo){
 	var anchoFranjaNumerica = (maximo-minimo) + 1;
 	var numero = Math.floor((Math.random() * anchoFranjaNumerica) + minimo);
@@ -12,11 +13,46 @@ function socioAleatorio(){
 
 	return nombresNegados[indice];
 }
+//definicion clases
 function Biblioteca(nombre){
 	this._nombre = nombre;
 	this._secciones = [];
 	this._socios = [];
 };
+
+Biblioteca.prototype.agregarSeccion=function(seccion){
+	this._secciones.push(seccion);
+}
+
+Biblioteca.prototype.construirBiblioteca=function(){
+//instanciar objetos
+var seccionAmor= new Seccion("Amor");
+var seccionAventuras= new Seccion("Aventuras");
+var seccionNaturaleza= new Seccion("Naturaleza");
+var seccionHistoria= new Seccion("Historia");
+var seccionViajes= new Seccion("Viajes");
+//se adiciona secciones
+bibliotecaDF.agregarSeccion(seccionAmor);
+bibliotecaDF.agregarSeccion(seccionAventuras);
+bibliotecaDF.agregarSeccion(seccionNaturaleza);
+bibliotecaDF.agregarSeccion(seccionHistoria);
+bibliotecaDF.agregarSeccion(seccionViajes);
+
+}
+Biblioteca.prototype.agregarSocio=function(socio){
+	this._socios.push(socio);
+}
+Biblioteca.prototype.generarSocios=function(){
+	for(indiceGenSocio=0;indiceGenSocio<100;indiceGenSocio++){
+		nombreSocio=socioAleatorio();
+		socioNuevo =new Socio(nombreSocio,indiceGenSocio);
+		this.agregarSocio(socioNuevo);
+	}
+};
+Biblioteca.prototype.ejecutarCiclo=function(){
+	this.generarSocios();
+}
+
 function Libro(nombre,paginas,autor,tematica){
 	this._nombre=nombre;
 	this._paginas=paginas;
@@ -32,6 +68,10 @@ function Seccion(nombre){
 function Socio(nombre,numeroSocio){
 	this._nombre=nombre;
 	this._numeroSocio=numeroSocio;
-	this.__librosRetirados[];
+	this._librosRetirados=[];
+};
 
-}
+var bibliotecaDF = new Biblioteca("Biblioteca Polanco");
+bibliotecaDF.construirBiblioteca();
+bibliotecaDF.ejecutarCiclo();
+
