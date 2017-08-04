7f9ade8676b2a0724f5445e12f8bbac0dfb3756b
Hasgard03 <daal8053@gmail.com>
2017-08-04 14:18:35 -0500

    avance
    avance

--- Examenes/001/examen.js
+++ Examenes/001/examen.js
@@ -7,11 +7,17 @@ function generarNumeroAleatorioEntre(minimo, maximo){
 	return numero;
 }
 
-function socioAleatorio(){
-	var nombresNegados = ["Carlos", "Daniel", "Fabian", "Juan Carlos", "Bryan", "Saul", "Christian", "Marcel", "Ronal", "David", "Fran"];
-	var indice = generarNumeroAleatorioEntre(0, nombresNegados.length-1);
+function nombreAleatorio(){
+	var nombres = ["Carlos", "Daniel", "Fabian", "Juan Carlos", "Bryan", "Saul", "Christian", "Marcel", "Ronal", "David", "Fran"];
+	var indice = generarNumeroAleatorioEntre(0, nombres.length-1);
 
-	return nombresNegados[indice];
+	return nombres[indice];
+}
+function tematicaAleatorio(){
+	var tematicas = ["Amor", "Aventuras", "Naturaleza", "Historia", "Viajes"];
+	var indice = generarNumeroAleatorioEntre(0, tematicas.length-1);
+
+	return tematicas[indice];
 }
 //definicion clases
 function Biblioteca(nombre){
@@ -44,20 +50,31 @@ Biblioteca.prototype.agregarSocio=function(socio){
 }
 Biblioteca.prototype.generarSocios=function(){
 	for(indiceGenSocio=0;indiceGenSocio<100;indiceGenSocio++){
-		nombreSocio=socioAleatorio();
-		socioNuevo =new Socio(nombreSocio,indiceGenSocio);
+		nombreSocio=nombreAleatorio();
+		socioNuevo =new Socio(nombreSocio,indiceGenSocio+1);
 		this.agregarSocio(socioNuevo);
 	}
 };
 Biblioteca.prototype.ejecutarCiclo=function(){
 	this.generarSocios();
-}
+	this.anadirLibro();
+	for(var indiceSeccion=0;indiceSeccion<this._secciones.length;indiceSeccion++){
+		this._secciones[indiceSeccion].ejecutarCiclo();
+	}
+};
+Biblioteca.prototype.anadirLibro=function(){
 
-function Libro(nombre,paginas,autor,tematica){
-	this._nombre=nombre;
-	this._paginas=paginas;
-	this._autor=autor;
-	this._tematica=tematica;
+	for(indiceGenlibros=0;indiceGenlibros<1000;indiceGenlibros++){
+		nombreLibro="Libro"+indiceGenlibros.toString();
+		paginasLibro=generarNumeroAleatorioEntre(100,500);
+		autor=nombreAleatorio();
+		//tematica=tematicaAleatorio();
+		SeccionAleatoria=generarNumeroAleatorioEntre(0,this._secciones.length-1);
+		seccion=this._secciones[SeccionAleatoria];
+		tematica=seccion._nombre;
+		nuevoLibro = new Libro(nombreLibro,paginasLibro,autor,tematica);
+		seccion.agregarLibro(nuevoLibro);
+	}
 
 };
 function Seccion(nombre){
@@ -65,11 +82,64 @@ function Seccion(nombre){
 	this._libros=[];
 
 };
+/*Seccion.prototype.ConstruirSecciones=function(){
+	for(indiceGenlibros=0;indiceGenlibros<1000;indiceGenlibros++){
+		nombreLibro="Libro"+sindiceGenlibros.toString();
+		paginasLibro=generarNumeroAleatorioEntre(100,500);
+		autor=nombreAleatorio();
+		tematica=tematicaAleatorio();
+		nuevoLibro = new Libro(nombreLibro,indiceGenlibros,paginasLibro,autor);
+		this.agregarLibro(nuevoLibro);
+	}
+
+};*/
+Seccion.prototype.agregarLibro=function(libro){
+
+	this._libros.push(libro);
+}
+
+Seccion.prototype.ejecutarCiclo=function(){
+	
+	//this.ConstruirSecciones();
+}
+function Libro(nombre,paginas,autor,tematica){
+	this._nombre=nombre;
+	this._paginas=paginas;
+	this._autor=autor;
+	this._tematica=tematica;
+
+};
 function Socio(nombre,numeroSocio){
 	this._nombre=nombre;
 	this._numeroSocio=numeroSocio;
 	this._librosRetirados=[];
 };
+Socio.prototype.devolverLibro=function(){
+
+var secciones=null;
+var libros=null;
+van entrega=0;
+secciones=bibliotecaDF._secciones;
+for(var indiSeccion=0; indiSeccion<secciones.length; indiSeccion++) {
+		libros=secciones[indiceBiblioteca]._libros;
+		entrega=generarNumeroAleatorioEntre(0,libros.length-1)
+		this._librosRetirados.push(libros[entrega]);
+		}
+	}
+
+}
+Socio.prototype.dameLibroAleatorio=function(){
+
+
+}
+Socio.prototype.ejecutarCiclo=function(){
+	if(this._librosRetirados.length-1<0){
+		this.dameLibroAleatorio();
+	}
+	else{
+		this.entregarLibro();
+		this.dameLibroAleatorio();
+}
 
 var bibliotecaDF = new Biblioteca("Biblioteca Polanco");
 bibliotecaDF.construirBiblioteca();
